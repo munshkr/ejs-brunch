0a9948a74d9fe238883ab0fa0ff299268cf67bb1
Damián Silvani <munshkr@gmail.com>
2016-08-03 21:24:01 -0300

    Use ejs.compile to generate a compilation function

--- index.js
+++ index.js
@@ -1,60 +1,30 @@
 'use strict';
 
-// Documentation for Brunch plugins:
-// https://github.com/brunch/brunch/blob/master/docs/plugins.md
+const ejs = require('ejs');
 
-// Remove everything your plugin doesn't need.
-class BrunchPlugin {
+class EjsPlugin {
   constructor(config) {
-    // Replace 'plugin' with your plugin's name;
     this.config = config && config.plugins && config.plugins.plugin;
   }
 
-  // file: File => Promise[Boolean]
-  // Called before every compilation. Stops it when the error is returned.
-  // Examples: ESLint, JSHint, CSSCheck.
-  // lint(file) { return Promise.resolve(true); }
-
   // file: File => Promise[File]
-  // Transforms a file data to different data. Could change the source map etc.
-  // Examples: JSX, CoffeeScript, Handlebars, SASS.
-  // compile(file) { return Promise.resolve(file); }
-
-  // file: File => Promise[Array: Path]
-  // Allows Brunch to calculate dependants of the file and re-compile them too.
-  // Examples: SASS '@import's, Jade 'include'-s.
-  // getDependencies(file) { return Promise.resolve(['dep.js']); }
-
-  // file: File => Promise[File]
-  // Usually called to minify or optimize the end-result.
-  // Examples: UglifyJS, CSSMin.
-  // optimize(file) { return Promise.resolve({data: minify(file.data)}); }
-
-  // files: [File] => null
-  // Executed when each compilation is finished.
-  // Examples: Hot-reload (send a websocket push).
-  // onCompile(files) {}
-
-  // Allows to stop web-servers & other long-running entities.
-  // Executed before Brunch process is closed.
-  // teardown() {}
+  // Generates a compilation function from a EJS template string
+  compile(file) {
+    let template;
+    try {
+      template = ejs.compile(file.data);
+    } catch(error) {
+      console.error(error);
+      return Promise.reject(error);
+    } finally {
+      console.log('done');
+      return Promise.resolve({template});
+    }
+  }
 }
 
-// Required for all Brunch plugins.
-BrunchPlugin.prototype.brunchPlugin = true;
+EjsPlugin.prototype.brunchPlugin = true;
+EjsPlugin.prototype.type = 'javascript';
+EjsPlugin.prototype.pattern = /\.ejs$/;
 
-// Required for compilers, linters & optimizers.
-// 'javascript', 'stylesheet' or 'template'
-BrunchPlugin.prototype.type = 'javascript';
-
-// Required for compilers & linters.
-// It would filter-out the list of files to operate on.
-//BrunchPlugin.prototype.extension = 'js';
-BrunchPlugin.prototype.pattern = /\.ejs$/;
-
-// Indicates which environment a plugin should be applied to.
-// The default value is '*' for usual plugins and
-// 'production' for optimizers.
-// BrunchPlugin.prototype.defaultEnv = 'production';
-
-module.exports = BrunchPlugin;
+module.exports = EjsPlugin;
--- test.js
+++ test.js
@@ -11,4 +11,22 @@ describe('Plugin', () => {
   it('should be an object', () => {
     expect(plugin).to.be.ok;
   });
+
+  it('should have a #compile method', () => {
+    expect(plugin.compile).to.be.an.instanceof(Function);
+  });
+
+  it('should compile and produce valid result', function(done) {
+    const content  = '<p><%= "hello, world!".toUpperCase() %> <%= locals.var %></p>';
+    const expected = '<p>HELLO, WORLD! foo</p>';
+
+    plugin.compile({data: content}).then(module => {
+      try {
+        expect(module.template({var: 'foo'})).to.equal(expected);
+        done();
+      } catch(err) {
+        done(err);
+      }
+    }, error => expect(error).not.to.be.ok);
+  });
 });
